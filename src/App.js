a35664a3f67bbacb6ff4af84cb44159937314ab6
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-13 12:26:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AccountSummary from './Components/AccountSummary';
+import Balance from './Components/Balance';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <Header></Header>
+    <div>
+      <Balance></Balance>
+      <br />
     </div>
+    <div>
+      <AccountSummary></AccountSummary>
+    </div>
+  </div>
   );
 }
 
