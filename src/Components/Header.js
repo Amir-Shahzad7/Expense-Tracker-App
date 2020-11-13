e3cc8ca7b1a69a3413c274130240dab355d317d8
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-13 12:26:25 +0500

    Create Header.js

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <p className="Header">
+            <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'></link>
+            Expense Tracker
+        </p>
+    )
+}
+
+export default Header;
