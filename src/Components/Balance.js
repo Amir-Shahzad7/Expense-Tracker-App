422f327626e253c08b717dc5e345a351fdf4e665
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-13 12:26:23 +0500

    Create Balance.js

---
+++ src/Components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Balance = () => {
+    return (
+        <div className="Balance">
+            <p><b>YOUR BALANCE</b></p>
+            <h1>$0.00</h1>
+        </div>
+    )
+}
+
+export default Balance
