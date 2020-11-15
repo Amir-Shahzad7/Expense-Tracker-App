cfa7efc16cbe3f2de1309c9196608d9c6633e40b
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-15 21:15:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import TransactionHistory from './Components/TransactionHistory';
 function App() {
   return (
   <div className="App">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     <Header></Header>
     <div>
       <Balance></Balance>
