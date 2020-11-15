1a38384c4df23aa34a887891a59510c0d052a35f
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-15 20:54:05 +0500

    Update AddTransaction.js

---
+++ src/Components/AddTransaction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddTransaction = () => {
+    return (
+        <div className="AddTransaction">
+            <h3>Add new transaction</h3>
+            <hr className="HorizontalLine"></hr>
+        </div>
+    )
+}
+
+export default AddTransaction;
