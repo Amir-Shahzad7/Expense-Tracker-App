b00ec591c41f4eca839020d8c47b4ef1d9939be5
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-15 20:54:14 +0500

    Update TransactionHistory.js

---
+++ src/Components/TransactionHistory.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TransactionHistory = () => {
+    return (
+        <div>
+            <div className="TransactionHistory">
+                <h3>History</h3>
+
+                <hr className="HorizontalLine"></hr>
+            </div>
+        </div>
+    )
+}
+
+export default TransactionHistory;
