f25b3e79322980831b371ee22e4f787487f8eca6
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-15 20:53:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import AccountSummary from './Components/AccountSummary';
+import AddTransaction from './Components/AddTransaction';
 import Balance from './Components/Balance';
 import Header from './Components/Header';
+import TransactionHistory from './Components/TransactionHistory';
 
 function App() {
   return (
   <div className="App">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     <Header></Header>
     <div>
       <Balance></Balance>
@@ -14,6 +17,13 @@ function App() {
     </div>
     <div>
       <AccountSummary></AccountSummary>
+      <br />
+    </div>
+    <div>
+      <TransactionHistory></TransactionHistory>
+    </div>
+    <div>
+      <AddTransaction></AddTransaction>
     </div>
   </div>
   );
