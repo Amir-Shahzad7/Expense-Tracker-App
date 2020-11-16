cb467dd588a318b248d8f0079f35ef46d34d52b7
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-16 13:44:23 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AccountSummary from './Components/AccountSummary';
 import AddTransaction from './Components/AddTransaction';
 import Balance from './Components/Balance';
+import Form from './Components/Form';
 import Header from './Components/Header';
 import TransactionHistory from './Components/TransactionHistory';
 
@@ -24,6 +25,9 @@ function App() {
       </div>
       <div>
         <AddTransaction></AddTransaction>
+      </div>
+      <div>
+        <Form></Form>
       </div>
     </div>
   );
