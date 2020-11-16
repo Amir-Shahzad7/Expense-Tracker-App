3976f11d6fc4db101b940bc701c6e2a6bce451a7
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-16 11:28:23 +0500

    Update AccountSummary.js

--- src/Components/AccountSummary.js
+++ src/Components/AccountSummary.js
@@ -7,9 +7,6 @@ const AccountSummary = () => {
                 <h4><b>INCOME</b></h4>
                 <p style={{ fontSize: "20px", color: "#2ECC71" }}>$0.00</p>
             </div>
-            <div style={{borderLeft: "1px solid #dedede", height: "40px"}}>
-
-            </div>
             <div>
                 <h4><b>EXPENSES</b></h4>
                 <p style={{ fontSize: "20px", color: "#C8392B" }}>$0.00</p>
