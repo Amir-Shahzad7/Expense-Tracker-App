a49e1394d8a9dae19399a458ed015dea42e34f71
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-16 13:57:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,26 +9,29 @@ import TransactionHistory from './Components/TransactionHistory';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <br />
-      <div>
-        <Balance></Balance>
-        <br />
-      </div>
-      <div>
-        <AccountSummary></AccountSummary>
-        <br />
-      </div>
-      <div>
-        <TransactionHistory></TransactionHistory>
-      </div>
-      <div>
-        <AddTransaction></AddTransaction>
-      </div>
-      <div>
-        <Form></Form>
-      </div>
+    <div className="App" >
+      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"></meta>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"></meta>
+          <meta name="HandheldFriendly" content="true"></meta>
+          <Header></Header>
+          <br />
+          <div>
+            <Balance></Balance>
+            <br />
+          </div>
+          <div>
+            <AccountSummary></AccountSummary>
+            <br />
+          </div>
+          <div>
+            <TransactionHistory></TransactionHistory>
+          </div>
+          <div>
+            <AddTransaction></AddTransaction>
+          </div>
+          <div>
+            <Form></Form>
+          </div>
     </div>
   );
 }
