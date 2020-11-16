87a5dcb082049cbf9a242a4e397dda9abebf02e1
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-11-16 15:30:34 +0500

    Update Balance.js

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Balance = () => {
     return (
         <div className="Balance">
-            <p><b>YOUR BALANCE</b></p>
-            <h1>$0.00</h1>
+            <p><b>YOUR BALANCE</b><br /><h1>$0.00</h1></p>
         </div>
     )
 }
