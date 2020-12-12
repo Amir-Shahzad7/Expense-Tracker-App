a9d2c9ff07852930fd781f89062b07c45cc8bf03
Amir Shahzad <amirshahzad.t7@gmail.com>
2020-12-12 16:12:47 +0500

    Formate the code

--- src/Context/GlobalContext.js
+++ src/Context/GlobalContext.js
@@ -1,47 +1,44 @@
-import React, { createContext, useReducer } from 'react';
-import TransactionReducer from '../Reducer/AppReducer';
+import React, { createContext, useReducer } from "react";
+import TransactionReducer from "../Reducer/AppReducer";
 
 // Creating Initial State.
 
-const initialTransactions = [
-]
+const initialTransactions = [];
 
-// Creating Global Context. 
-
+// Creating Global Context.
 
 export const GlobalContext = createContext(initialTransactions);
 
-
-// Creating GlobalProvider. 
+// Creating GlobalProvider.
 
 export const GlobalProvider = ({ children }) => {
-    let [state, dispatch] = useReducer(TransactionReducer, initialTransactions);
-    function addTransactions(transObj) {
-        dispatch({
-            type: "AddTransactions",
-            payload: {
-                text: transObj.text,
-                amount: transObj.amount,
-                id: transObj.id,
-            }
-        });
-    }
-    function deleteTransactions(id) {
-        dispatch({
-            type: "DeleteTransactions",
-            payload: id,
-        })
-    }
-    return (
-        <GlobalContext.Provider value={
-            {
-                transactions: state,
-                addTransactions,
-                deleteTransactions,
-            }
-        }> {children}
-        </GlobalContext.Provider>
-    )
-}
-
-
+  let [state, dispatch] = useReducer(TransactionReducer, initialTransactions);
+  function addTransactions(transObj) {
+    dispatch({
+      type: "AddTransactions",
+      payload: {
+        text: transObj.text,
+        amount: transObj.amount,
+        id: transObj.id,
+      },
+    });
+  }
+  function deleteTransactions(id) {
+    dispatch({
+      type: "DeleteTransactions",
+      payload: id,
+    });
+  }
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state,
+        addTransactions,
+        deleteTransactions,
+      }}
+    >
+      {" "}
+      {children}
+    </GlobalContext.Provider>
+  );
+};
